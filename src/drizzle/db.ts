9fec01caf75d706d8109ad761c97e0b93ebcf8c3
Yash-Tibadia <tibadiyayash@gmail.com>
2025-11-26 14:23:54 +0530

    feat: add drizzle configuration and database connection
    - Created drizzle.config.ts for migration and schema management.
    - Added config.ts to manage database URL from environment variables.
    - Implemented db.ts to establish a connection to the Neon database using drizzle ORM.
    - Defined schema export in schema.ts.
    - Initialized users schema in users.ts.

---
+++ src/drizzle/db.ts
@@ -0,0 +1,7 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+import { config } from "@/config/config";
+
+const sql = neon(config.databaseUrl!);
+const db = drizzle({ client: sql, schema });
